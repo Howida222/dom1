77681966772c1f6aa6beb8263a751a86af233255
Howida222 <wda192005@gmail.com>
2024-09-03 00:43:32 +0300

    Add files via upload

---
+++ css/js/main.js
@@ -0,0 +1,11 @@
+document.querySelector("#red").onclick =function(){
+    document,this.querySelector("#result").computedStyleMap.color="red";
+
+}
+document.querySelector("#green").onclick =function(){
+    document,this.querySelector("#result").computedStyleMap.color="green";
+    
+}
+document.querySelector("#blue").onclick= function(){
+    document.querySelector("#result").computedStyleMap.color="blue";
+}
---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector("#red").onclick = function() {
+    document.querySelector("#result").style.color = "red";
+}
+document.querySelector("#green").onclick = function() {
+    document.querySelector("#result").style.color = "green";
+}
+document.querySelector("#blue").onclick = function() {
+    document.querySelector("#result").style.color = "blue";
+}
